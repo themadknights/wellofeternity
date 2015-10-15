35e8139f3eeea9ad150d790deafbb2b85de0c0ac
Reik Val <guardian_de_las_sombras@hotmail.com>
2015-10-15 16:30:43 +0200

    Platform collision and player health

--- src/js/sprites/player.js
+++ src/js/sprites/player.js
@@ -3,5 +3,12 @@ export class Player extends Phaser.Sprite {
         super(game, x, y, 'player');
         this.anchor.setTo(0.5);
         this.game.add.existing(this);
+        this.FALL_SPEED_LIMIT = 300;
+        this.health = 3;
+        this.tooFast = false;
+    }
+
+    loseHealth(health) {
+        this.health -= health;
     }
 }
--- src/js/states/start.js
+++ src/js/states/start.js
@@ -17,16 +17,32 @@ export class StartState extends Phaser.State {
         //Player
         this.player = new Player(this.game, this.game.world.centerX, this.game.world.centerY);
         this.physics.arcade.enable(this.player);
+        this.camera.follow(this.player);
 
         //Creating the map and its main layer, resizering the world to fix that layer
         this.map = this.add.tilemap();
         this.map.addTilesetImage('world');
         this.platforms = this.map.create('platforms', 25, 100, 32, 32);
         this.platforms.resizeWorld();
+        this.map.setCollision(0);
 
         //TODO: Example of platform, to be deleted when the map generation is done
         for(i = 0; i < 15; i++) {
             this.map.putTile(0, 10+i, 15);
         }
+    }
+
+    update() {
+        if(this.player.body.velocity.y > this.player.FALL_SPEED_LIMIT) {
+            this.player.tooFast = true;
+        } else {
+            this.player.tooFast = false;
+        }
+
+        this.physics.arcade.collide(this.player, this.platforms, function(player) {
+            if(player.tooFast) {
+                player.loseHealth(3);
+            }
+        });
     }
 }
