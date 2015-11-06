a655866ea75b18aca95534c238f90810745b7934
Reik Val <guardian_de_las_sombras@hotmail.com>
2015-11-06 21:44:08 +0100

    Now the world works properly with 20 tiles width

--- src/js/states/start.js
+++ src/js/states/start.js
@@ -40,12 +40,12 @@ export class StartState extends Phaser.State {
         this.mapPresets = this.game.cache.getJSON('presets');
         this.map = this.add.tilemap();
         this.map.addTilesetImage('world');
-        this.platforms = this.map.create('platforms', 25, 100, 32, 32);
+        this.platforms = this.map.create('platforms', 20, 100, 32, 32);
         this.generateWorldChunk(20);
         this.map.setCollisionBetween(0, 5);
 
         //TODO: Example of platform, to be deleted when the map generation is done
-        for(i = 0; i < 15; i++) {
+        for(i = 0; i < 10; i++) {
             this.map.putTile(i%6, 10+i, 15);
             this.map.putTile(i%6+6, 10+i, 16);
         }
@@ -60,16 +60,9 @@ export class StartState extends Phaser.State {
         }, this);
 
         //TODO: Example of spikes, to be deleted when the map generation is done
-        //The player can walk over spikes if they are 2 and he walks quickly
         this.map.putTile(12, 15, 15);
         this.map.putTile(12, 16, 15);
-        //The player can't fall <--- Maybe, the hitbox of the player should be smaller.
         this.map.putTile(12, 18, 15);
-        //The player die
-        this.map.putTile(12, 20, 15);
-        this.map.putTile(12, 21, 15);
-        this.map.putTile(12, 22, 15);
-        //The player can walk through spikes
         this.map.putTile(12, 10, 14);
         //Replace 12 index for 12..15 randomly
         this.replaceRandomSpikes();
@@ -83,7 +76,7 @@ export class StartState extends Phaser.State {
         }, this);
 
         //TODO: Example of goal, to be deleted when the map generation si done
-        for(i = 0; i < 25; i++) {
+        for(i = 0; i < 20; i++) {
             this.map.putTile(98, i, 99);
         }
 
@@ -220,9 +213,9 @@ export class StartState extends Phaser.State {
 
     generateWorldChunk(y) {
         var mapChunk = this.mapPresets.data[this.game.rnd.integerInRange(0,2)];
-        var i = 0;
+        var i = -1;
         this.map.forEach(function(tile) {
             tile.index = mapChunk[i++] - 1;
-        }, this, 0, y, 25, 20);
+        }, this, 0, y, 20, 20);
     }
 }
