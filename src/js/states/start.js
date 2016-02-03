1079794401cadb2278fb2df9bd6e8e0607705b0d
beagleknight <david.morcillo@gmail.com>
2016-02-03 20:20:58 +0100

    Finish start scene and passing level id

--- src/js/states/start.js
+++ src/js/states/start.js
@@ -12,6 +12,7 @@ export class StartState extends Phaser.State {
         this.panels = {};
         this.createStartPanel();
         this.createModePanel();
+        this.createLevelsPanel();
     }
 
     togglePanel(panelId) {
@@ -21,10 +22,14 @@ export class StartState extends Phaser.State {
         this.panels[panelId].visible = true;
     }
 
+    startLevel(levelId) {
+        this.game.state.start('level', true, false, levelId);
+    }
+
     createStartPanel() {
         let panel = this.game.add.group();
 
-        panel.position = { x: this.game.world.centerX, y: this.game.world.centerY + 50 };
+        panel.position = { x: this.game.world.centerX, y: this.game.world.centerY + 25 };
 
         this.startButton = new TextButton({
             game: this.game,
@@ -38,7 +43,8 @@ export class StartState extends Phaser.State {
             game: this.game,
             group: panel,
             position: { x: 0, y: 125 },
-            text: "Options"
+            text: "Options",
+            disabled: true
         });
 
         this.panels["start"] = panel;
@@ -47,32 +53,77 @@ export class StartState extends Phaser.State {
     createModePanel() {
         let panel = this.game.add.group();
 
-        panel.position = { x: this.game.world.centerX, y: this.game.world.centerY + 50 };
+        panel.position = { x: this.game.world.centerX, y: this.game.world.centerY + 25 };
         panel.visible = false;
 
         this.normalModeButton = new TextButton({
             game: this.game,
             group: panel,
             position: { x: 0, y: 0 },
-            text: "Normal Mode"
+            text: "Normal Mode",
+            callback: () => { this.togglePanel('level'); }
         });
 
         this.infiniteModeButton = new TextButton({
             game: this.game,
             group: panel,
             position: { x: 0, y: 125 },
-            text: "Infinte Mode"
+            text: "Infinte Mode",
+            callback: () => { this.startLevel('infinite'); }
         });
 
         this.infiniteModeButton = new TextButton({
             game: this.game,
             group: panel,
-            position: { x: -225, y: 325 },
+            position: { x: -225, y: 350 },
             scale: 0.5,
             text: "Back",
             callback: () => { this.togglePanel('start'); }
         });
 
         this.panels["mode"] = panel;
+    }
+
+    createLevelsPanel() {
+        let panel = this.game.add.group();
+
+        panel.position = { x: this.game.world.centerX, y: this.game.world.centerY + 25 };
+        panel.visible = false;
+
+        this.easyLevelButton = new TextButton({
+            game: this.game,
+            group: panel,
+            position: { x: 0, y: 0 },
+            text: "Easy",
+            callback: () => { this.startLevel('easy'); }
+        });
+
+        this.normalLevelButton = new TextButton({
+            game: this.game,
+            group: panel,
+            position: { x: 0, y: 125 },
+            text: "Normal",
+            callback: () => { this.startLevel('normal'); }
+        });
+
+        this.insaneLevelButton = new TextButton({
+            game: this.game,
+            group: panel,
+            position: { x: 0, y: 250 },
+            text: "Insane",
+            callback: () => { this.startLevel('insane'); }
+        });
+
+        this.infiniteModeButton = new TextButton({
+            game: this.game,
+            group: panel,
+            position: { x: -225, y: 350 },
+            scale: 0.5,
+            text: "Back",
+            callback: () => { this.togglePanel('mode'); }
+        });
+
+
+        this.panels["level"] = panel;
     }
 }
--- src/js/ui/text_button.js
+++ src/js/ui/text_button.js
@@ -1,11 +1,15 @@
 export class TextButton {
-    constructor({ game, group, position, text, callback, scale }) {
-        this.button = game.add.button(position.x, position.y, 'buttonBg', callback);
+    constructor({ game, group, position, text, callback, scale, disabled }) {
+        this.button = game.add.button(position.x, position.y, 'buttonBg', disabled ? () => {} : callback);
         this.button.anchor.setTo(0.5);
         group.add(this.button);
 
         if (scale) {
             this.button.scale.setTo(scale);
+        }
+
+        if (disabled) {
+            this.button.alpha = 0.25;
         }
 
         this.label = game.add.bitmapText(position.x, position.y, 'carrier_command', text, 20);
